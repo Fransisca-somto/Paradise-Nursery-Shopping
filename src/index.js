9263fbb96dccf9cffbd65c67224f1bfd4282cd9b
Cisca <106766517+Fransisca-somto@users.noreply.github.com>
2025-07-14 10:07:58 +0000

    Rechecked

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store";
-import Home from "./components/Homepage"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />)
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
 
 
 
