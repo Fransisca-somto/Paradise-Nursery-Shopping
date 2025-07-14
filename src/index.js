6a99868fe045e5aff83c2ba9cf093389abf7aec5
Cisca <106766517+Fransisca-somto@users.noreply.github.com>
2025-07-14 09:47:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store";
+import Home from "./components/Homepage"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
-        <App />
+        <Home />
     </Provider>
 )
 
